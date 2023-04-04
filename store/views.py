0545345108770021da24ef553479a9b85e294847
An Pham <an.phxm@gmail.com>
2023-04-04 16:47:05 -0400

    Formatted cart page and checkout page

--- store/views.py
+++ store/views.py
@@ -27,11 +27,16 @@ def products(request):
     context = {'products':products}
     return render(request, 'store/products.html', context)
 
-
-def cart(request):
-    context = {}
-    return render(request, 'store/cart.html', context)
-
 def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html', context)
+	
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+	context = {'items':items, 'order': order}
+	return render(request, 'store/checkout.html', context)
+	
